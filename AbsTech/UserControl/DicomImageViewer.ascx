71762d84a5fe4282309f8d344f656cd4f5d8fbf4
Subrato <Subrato@LAPTOP-72MLSIA2>
2018-07-10 20:51:19 +0530

    dcm file added

---
+++ AbsTech/UserControl/DicomImageViewer.ascx
@@ -0,0 +1,4 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="DicomImageViewer.ascx.cs" Inherits="AbsTech.UserControl.DicomImageViewer" %>
+<asp:Panel ID="pnlImageViewer" runat="server" Height="639px" >
+
+</asp:Panel>
---
+++ AbsTech/UserControl/DicomImageViewer.ascx.cs
@@ -0,0 +1,17 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AbsTech.UserControl
+{
+    public partial class DicomImageViewer : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
