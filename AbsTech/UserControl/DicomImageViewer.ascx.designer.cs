e9d202832cb946a21ad5310f75dedbe7b267d348
Subrato <Subrato@LAPTOP-72MLSIA2>
2018-07-27 21:34:16 +0530

    wado loader

--- AbsTech/UserControl/DicomImageViewer.ascx.designer.cs
+++ AbsTech/UserControl/DicomImageViewer.ascx.designer.cs
@@ -11,14 +11,5 @@ namespace AbsTech.UserControl {
     
     
     public partial class DicomImageViewer {
-        
-        /// <summary>
-        /// pnlImageViewer control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Panel pnlImageViewer;
     }
 }
